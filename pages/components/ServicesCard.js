d2b04bb5b249d329735ab008413e28fab5b658fb
Aaron Robinsom <airobinson0025@gmail.com>
2023-06-04 16:32:54 -0400

    chore: chores

--- pages/components/ServicesCard.js
+++ pages/components/ServicesCard.js
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 const ServicesCard = ({id, img, title, description}) => {
   return (
-    <div className='flex flex-col items-center font-montserrat w-[520px]'>
+    <div className='flex flex-col items-center font-montserrat'>
       <div>
-        <Image src={img} width={520} height={520} alt='image' className='w-auto h-auto' />
+        <Image src={img} width={520} height={520} alt='image' className='w-auto h-auto'  priority/>
       </div>
       <div className='flex flex-col text-center space-y-6 mt-16 w-[400px]'>
         <h1 className='text-[1.35rem] font-semibold tracking-wide'>{title}</h1>
--- pages/components/ValueCard.js
+++ pages/components/ValueCard.js
@@ -5,7 +5,7 @@ export const ValueCard = ({img, title, text}) => {
   return (
     <div className='flex flex-col items-center space-y-16'>
         <div className='mx-20'>
-            <Image src={img} alt={title} width={700} height={550} className='w-auto h-auto rounded-sm' />
+            <Image src={img} alt={title} width={700} height={550} className='w-auto h-auto rounded-sm' priority/>
         </div>
                       
         <div className='flex flex-col items-center text-center space-y-6 px-10'>
